181da639406485315d4ffba2a161d78918e8dc3c
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-02-28 15:07:16 +1300

    Add movie card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,20 @@ export class AppComponent {
   constructor(public dialog: MatDialog) {}
 
   openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent,{
-      width: '280px'
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px',
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px',
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: ''};
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        this.dialogRef.close();
+        console.log(result);
+        localStorage.setItem('user', result.user.Username)
+        localStorage.setItem('token', result.token)
+        this.snackBar.open('User login successfully!', 'OK', {
+          duration: 2000,
+        });
+      },
+      (result) => {
+        console.log(result);
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+
+}
